2c51d9548e63058b760653f90ec791ef0dce1950
rayrayc786 <60880701+rayrayc786@users.noreply.github.com>
2021-03-30 00:51:13 +0530

    Create FloodFill.java

---
+++ FloodFill.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        boolean [][] visited=new boolean[n][n];
+        floodfill(arr,0,0,"",visited);
+    }
+public static void floodfill(int[][] maze,int sr,int sc,String psf,boolean[][] visited) {
+    if(sr<0 || sr>maze.length-1 || sc<0 || sc>maze[0].length-1 || visited[sr][sc]==true || maze[sr][sc]==1){
+        return;
+    }
+    if(sr==maze.length-1 && sc==maze.length-1){
+        System.out.println(psf);
+        return;
+    }
+    visited[sr][sc]=true;
+    floodfill(maze,sr-1,sc,psf+"t",visited);
+    floodfill(maze,sr,sc-1,psf+"l",visited);
+    floodfill(maze,sr+1,sc,psf+"d",visited);
+    floodfill(maze,sr,sc+1,psf+"r",visited);    
+    visited[sr][sc]=false;
+}
+}
