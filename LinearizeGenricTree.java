2cc5c79a74c95064ee0f5bef15bb1dd124ba90ea
rayrayc786 <60880701+rayrayc786@users.noreply.github.com>
2021-04-13 06:27:51 +0530

    Update LinearizeGenricTree.java

--- LinearizeGenricTree.java
+++ LinearizeGenricTree.java
@@ -159,6 +159,20 @@ public class Main {
         node.children.remove(i+1);
     }
   }
+  //efficient approach O(n)
+  public static Node linearize2(Node node){
+        if(node.children.size()==0){
+            return node;
+        }
+        Node lkt=linearize2(node.children.get(node.children.size()-1));
+        while(node.children.size()>1){
+            Node last=node.children.remove(node.children.size()-1);
+            Node sl=node.children.get(node.children.size()-1);
+            Node slkt=linearize2(sl);
+            slkt.children.add(last);
+        }
+        return lkt;
+    }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
